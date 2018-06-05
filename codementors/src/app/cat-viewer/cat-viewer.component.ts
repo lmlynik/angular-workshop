5d07afb1a81d6cc0969d35c5c934f0ae4a9f8bf2
lmlynik <lukasz.mlynik@hotmail.com>
2018-06-05 14:04:52 +0200

    Input and interpolation

--- codementors/src/app/cat-viewer/cat-viewer.component.ts
+++ codementors/src/app/cat-viewer/cat-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cat-viewer',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatViewerComponent implements OnInit {
 
+  @Input() name: string;
   constructor() { }
 
   ngOnInit() {
