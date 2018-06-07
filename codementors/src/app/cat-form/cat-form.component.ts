663bfaccf4b27007a3c8b61db8f51f8139886abf
lmlynik <lukasz.mlynik@hotmail.com>
2018-06-07 17:53:24 +0200

    Fixed component

--- codementors/src/app/cat-form/cat-form.component.ts
+++ codementors/src/app/cat-form/cat-form.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CatService } from '../cat.service';
+import { Component, OnInit } from "@angular/core";
+import { CatService } from "../cat.service";
 
 @Component({
-  selector: 'app-cat-form',
-  templateUrl: './cat-form.component.html',
-  styleUrls: ['./cat-form.component.css']
+  selector: "app-cat-form",
+  templateUrl: "./cat-form.component.html",
+  styleUrls: ["./cat-form.component.css"]
 })
 export class CatFormComponent implements OnInit {
   name: string;
   img: string;
-  constructor(private catService: CatService) { }
+  constructor(private catService: CatService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  add() {
+    this.catService.addCat({
+      name: this.name,
+      img: this.img,
+      foods: []
+    });
   }
-
-  add(){
-    this.catService.addCat({name:this.name, img: this.img});
-  }
-
 }
